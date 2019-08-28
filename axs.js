613ce734025fb0d4f737375131bac02a6e002675
Serhii Delihioz <dserhiy@gmail.com>
2019-08-28 20:59:30 +0300

    Fix minor bugs

--- axs.js
+++ axs.js
@@ -1,43 +1,37 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
 
-const vsldb = [
-  {
-    "name": "Sea-Lady",
-    "type": "Bulk Carrier",
-    "imo": "9266188",
-    "callsign": "V7XH9",
-    "mmsi": "538004485"
-  }, {
-    "name": "Sea-Lady-Ii",
-    "type": "Yacht",
-    "imo": "9096703",
-    "callsign": "9HB2175",
-    "mmsi": "215628000"
-  }, {
-    "name": "Sealand-Atlantic",
-    "type": "Container Ship",
-    "imo": "8212685",
-    "callsign": "V7RH2",
-    "mmsi": "538003504"
-  },
-];
+const vsldb = JSON.parse(fs.readFileSync('./fleet.json'));
 
-getDesc(9096703);
+fillVslDescription(vsldb);
 
-function getDesc(imo) {
-  let url = `https://axsdry.axsmarine.com/common/server/controller_tech_data.php?_dc=1566941153072&out=echo&encoder=json&IMO=${imo}&launch=get_vessel_tech_desc`;
-  fetch(url , 
+function fillVslDescription(fleet, vslIndex = 0) {
+  let url = `https://axsdry.axsmarine.com/common/server/controller_tech_data.php?_dc=1566941153072&out=echo&encoder=json&IMO=${fleet[vslIndex].imo}&launch=get_vessel_tech_desc`;
+  const cookie = 'cuvid=cc1400b0579a4acf8ffee929919c37a0; PHPSESSID=a7258933c1760686904af12e524411d7; axs-webfarm=s1; cusid=1566999213117; cuvon=1566999217147; _oidc_t=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InNpZy1ycy0wIn0.eyJzdWIiOiJtaWtlcnllOEBvdXRsb29rLmNvbSIsIm5vbmNlIjoiYWEwYjRmMDBmZWJhOWIzN2UzY2JjMzliZWQxMDViYzAiLCJhdF9oYXNoIjoiVTlOMW5CbkZJNi1yUFNON1NyZE03QSIsInNpZCI6IjRhNmM3ZGMzLTM2M2EtNDdlMC1iMjE0LTFiNzU4YmM5N2I2MCIsImF1ZCI6IkF4c0RyeSIsImV4cCI6MTU2NzAwMjgzOCwiaWF0IjoxNTY2OTk5MjM4LCJpc3MiOiJodHRwczovL3BvcnRhbC5heHNtYXJpbmUuY29tIn0.CZaLs0SZ87FYtYm8n1hJeJpHPWIi0OaNc9wCvelJ7Sfh842sv9ls5XygIY3Bxg9Dri7GnFl5PDiMjPvrEU9IEQKp08bj33JN1VsenVWIL7DMB0-hJr-RTWSwpN1qlasB8IezBM2TBE_rVbDdwrnQNUWvyZUHnworN0JF_2f-9vqcIv-QhFcWK9mxkcUfG2Ivbju0VJunWum07LMQVMX4V89LhOiARkBENDNc8t0CF2sZdcoIG6Z3TeZgr5su35snQEX4FfEZEZGI164zKhw4goyLGux_e4JX3YcpP6IaS48fniTBvL6UL-uwwVV4yQwng8ChCv9yvZhhEgYSPYlzsw; axsmarineV4=ebff1b9f13f6d387edc25b5053c7d9f09d759d3b; site24x7rumID=489770796246437.1566999242242'
+  
+  fetch(url, 
     { 
       headers: {
-      'Cookie': 'axs-webfarm=s1; cusid=1566941528458; cuvid=c56c6b363e784d3cbdf20d8998884fac; PHPSESSID=2cc598a2db09c6ff584060738635ac91; cuvon=1566942118400; _oidc_t=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InNpZy1ycy0wIn0.eyJzdWIiOiJtaWtlcnllOEBvdXRsb29rLmNvbSIsIm5vbmNlIjoiYzNmYzMyMDI1Y2IwNGRkYjdiNTE2M2RkMGE4N2Q0YjUiLCJhdF9oYXNoIjoieTJCbVlzbHNDYmJGTGttUlFneGlQdyIsInNpZCI6ImYzNzEyNTEyLTZmZDItNGIxYi1hMWMzLTY4YzI2MzE5Mzc5ZCIsImF1ZCI6IkF4c0RyeSIsImV4cCI6MTU2Njk0NTc0NSwiaWF0IjoxNTY2OTQyMTQ1LCJpc3MiOiJodHRwczovL3BvcnRhbC5heHNtYXJpbmUuY29tIn0.dIH4bSyoGElLQ2EeLnQMIwkjQw5ntnJspywa80qyEBeHRBwpBsWo2uksLxl5ygylxskiCOEAdgfHbOi9_atIgOZswHlAwvexV8zcLZ7yIrFmDM_T5rucas6Luzyv0zBQ9mpxII_SEyQV1bYhmkuf_EY6WBtZWDpiTy3QNmLfGNmSyQxvo_ejH5k42IIFQVyla4KmWmA7h8smiIwd3NfQ3DFbBs8juWwyNdzh7lOa7OQgSqzixfFhaiLbOx-WHYLcJDFFDsPVNCwDnljdojFR7E7bkz7o2iJFo_vn6nXI7uCtSsMaZW8Cl3Kgw8kcSirAhBX92KN7sKwC9Ytoyd3iDg; axsmarineV4=72fa02b6b67c90f5cccf1d1f6538c5d6286bec27; site24x7rumID=222896808513945.1566942165856'
-    }
-  })
-    .then(res => res.text())
-    .then(text => {
-
-      console.log(text);
-       
-    });
+      'Cookie': cookie
+    }})
+    .then(res => res.json())
+    .then(data => {
+      if(data === 'failed')
+        console.log('data failed')
+        
+      fleet[vslIndex].description = data.vdDesc;
+      
+      if(vslIndex === fleet.length - 1) {
+        
+        fs.writeFile("fleet_desc.json", JSON.stringify(fleet), err => {
+          if (err) throw err;
+          console.log('The file has been saved!');
+        });
+      }
+      else {
+        fillVslDescription(fleet, vslIndex + 1);
+      }    
+    })
+    .catch(err => console.log(err));
 }
 
