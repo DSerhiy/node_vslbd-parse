5d231f062276f4e68d44e0abf880244392365e1c
Serhii Delihioz <DSerhiyWeb@gmail.com>
2019-09-04 23:15:41 +0300

    Fix minor bugs

--- descParser.js
+++ descParser.js
@@ -1,63 +1,33 @@
 const fs = require("fs");
 
-const vsls = JSON.parse(fs.readFileSync('./vsl-descr/fleet_B.json'));
+const vsls = JSON.parse(fs.readFileSync('./vsl-descr/fleet_D.json'));
 
 let updatedVsls = vsls.map((vsl, index) => {
 
   console.log(index, vsl.imo);
   
   const descText = vsl.description;
-  const nameMatch = descText.match(/<B>MV<\/B>\s"([\d\w\s\-\.]*)"/);
+  const name = parseText(descText, /<B>MV<\/B>\s"([\d\w\s\-\.']*)"/);
   
-  if(nameMatch[1] === '')
+  if(name === '')
     return {imo: 'delete'}
   
-  const name = nameMatch[1].trim();
-  const type = descText.match(/<B>Type :<\/B>([\w\s]*)</)[1].trim();
-  const subType = descText.match(/<B>Sub type :<\/B>([\w\s\-\/]*)</)[1].trim();
-  const flag = descText.match(/<B>Flag :<\/B>([\w\s\(\)&]*)</)[1].trim();
+  const type = parseText(descText, /<B>Type :<\/B>([\w\s]*)</);  
+  const subType = parseText(descText, /<B>Sub type :<\/B>([\w\s\-\/]*)</);  
+  const flag = parseText(descText, /<B>Flag :<\/B>([\w\s\(\)&]*)</);
 
   // const gears = descText.match(/<B>Gears :<\/B>([\s\w\d\.]*)<B>/)[1].trim();
 
-  const vslClassMatch = descText.match(/<B>Class :<\/B>([\w\s]*)</);
-  let vslClass = 0;
-  if(vslClassMatch) 
-    vslClass = vslClassMatch[1].trim();  
-  
-  const dwt = descText.match(/<B>DWT\/Draft:<\/B>([\d\,\s]*)M/)[1].trim();
-  const draft = descText.match(/<B>DWT\/Draft:<\/B>[\d\,\s]*MT DWT \/([\d\.\s]*) m/)[1].trim();
-
-  const tpcMatch = descText.match(/TPC \/ TPI :([\d\.\s]*)MT/);
-  let tpc = 0;
-  if(tpcMatch) 
-    tpc = tpcMatch[1].trim();   
-
-  const loaMatch = descText.match(/([\d\.]*) m \(loa\)/);
-  let loa = 0;
-  if(loaMatch) 
-    loa = loaMatch[1].trim();
-
-  const beamMatch = descText.match(/([\d\.]*) m \(beam\)/);  
-  let beam = 0;
-  if(beamMatch) 
-    beam = beamMatch[1].trim();
-
-  const lbpMatch = descText.match(/LBP :\s*([\d\.]*) m/);
-  let lbp = 0;
-  if(lbpMatch) 
-    lbp = lbpMatch[1].trim();     
-
-  const grt = descText.match(/Int'l tonnage : ([\d\,]*) GT/)[1].trim();  
-
-  const nrtMatch = descText.match(/Int'l tonnage : [\d\,]* GT \/ ([\d\,]*) NT/);
-  let nrt = 0;
-  if(nrtMatch) 
-    nrt = nrtMatch[1].trim();   
-
-  const shipownerMatch = descText.match(/Shipowner :([\w\s&\-]*)\s\-{2}/);
-  let shipowner = 0;
-  if(shipownerMatch) 
-    shipowner = shipownerMatch[1].trim();   
+  const vslClass = parseText(descText, /<B>Class :<\/B>([\w\s]*)</);
+  const dwt = parseText(descText, /<B>DWT\/Draft:<\/B>([\d\,\s]*)M/);
+  const draft = parseText(descText, /<B>DWT\/Draft:<\/B>[\d\,\s]*MT DWT \/([\d\.\s]*) m/);
+  const tpc = parseText(descText, /TPC \/ TPI :([\d\.\s]*)MT/);
+  const loa = parseText(descText, /([\d\.]*) m \(loa\)/);
+  const beam = parseText(descText, /([\d\.]*) m \(beam\)/);  
+  const lbp = parseText(descText, /LBP :\s*([\d\.]*) m/);
+  const grt = parseText(descText, /Int'l tonnage : ([\d\,]*) GT/);  
+  const nrt = parseText(descText, /Int'l tonnage : [\d\,]* GT \/ ([\d\,]*) NT/);
+  const shipowner = parseText(descText, /Shipowner :([\w\s&\-]*)\s\-{2}/);
 
   console.log(index, name);
     
@@ -87,8 +57,16 @@ let updatedVsls = vsls.map((vsl, index) => {
 updatedVsls = updatedVsls.filter(vsl => vsl.imo !== 'delete');
 console.log(updatedVsls.length);
 
-fs.writeFile("./vsl-descr/parsed/fleet_B.json", JSON.stringify(updatedVsls), err => {
+fs.writeFile("./vsl-descr/parsed/fleet_D.json", JSON.stringify(updatedVsls), err => {
   if (err) throw err;
   
   console.log('The file has been saved!');
 });
+
+function parseText(text, regexp) {
+  const match = text.match(regexp);
+  if(match) 
+    return match[1].trim();
+    
+  return 0;
+}
