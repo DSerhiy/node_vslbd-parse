f0544ad848d55a4c93c300230f7709eda8ff3304
Serhii Delihioz <DSerhiyWeb@gmail.com>
2019-09-06 22:15:54 +0300

    Fix minor changes

--- descParser.js
+++ descParser.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 
-const vsls = JSON.parse(fs.readFileSync('./vsl-descr/fleet_D.json'));
+let vsls = JSON.parse(fs.readFileSync('./vsl-descr/fleet_A.json'));
+
+vsls = vsls.filter(vsl => {
+  const name = parseText(vsl.description, /<B>MV<\/B>\s"([\d\w\s\-\.']*)"/);
+  if(name !== '')
+    return true
+});
 
 let updatedVsls = vsls.map((vsl, index) => {
 
   console.log(index, vsl.imo);
   
   const descText = vsl.description;
+  
   const name = parseText(descText, /<B>MV<\/B>\s"([\d\w\s\-\.']*)"/);
-  
-  if(name === '')
-    return {imo: 'delete'}
-  
   const type = parseText(descText, /<B>Type :<\/B>([\w\s]*)</);  
   const subType = parseText(descText, /<B>Sub type :<\/B>([\w\s\-\/]*)</);  
   const flag = parseText(descText, /<B>Flag :<\/B>([\w\s\(\)&]*)</);
-
-  // const gears = descText.match(/<B>Gears :<\/B>([\s\w\d\.]*)<B>/)[1].trim();
-
   const vslClass = parseText(descText, /<B>Class :<\/B>([\w\s]*)</);
   const dwt = parseText(descText, /<B>DWT\/Draft:<\/B>([\d\,\s]*)M/);
   const draft = parseText(descText, /<B>DWT\/Draft:<\/B>[\d\,\s]*MT DWT \/([\d\.\s]*) m/);
@@ -29,13 +29,15 @@ let updatedVsls = vsls.map((vsl, index) => {
   const nrt = parseText(descText, /Int'l tonnage : [\d\,]* GT \/ ([\d\,]*) NT/);
   const shipowner = parseText(descText, /Shipowner :([\w\s&\-]*)\s\-{2}/);
 
+  const gears = descText.includes('Gearless')? 'Gearless': 'Geared';
+
   console.log(index, name);
     
   return {
     name,
     type,
     subType, 
-    // gears,
+    gears,
     imo: vsl.imo,
     callsign: vsl.callsign,
     mmsi: vsl.mmsi,
@@ -54,10 +56,9 @@ let updatedVsls = vsls.map((vsl, index) => {
   }
 });
 
-updatedVsls = updatedVsls.filter(vsl => vsl.imo !== 'delete');
 console.log(updatedVsls.length);
 
-fs.writeFile("./vsl-descr/parsed/fleet_D.json", JSON.stringify(updatedVsls), err => {
+fs.writeFile("./vsl-descr/parsed/fleet_A.json", JSON.stringify(updatedVsls), err => {
   if (err) throw err;
   
   console.log('The file has been saved!');
