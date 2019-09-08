cc963c1b4693eb784bbb1ba6688c7534dd0c0f89
Serhii Delihioz <DSerhiyWeb@gmail.com>
2019-09-08 23:05:26 +0300

    Version 1.0.0 descParser

--- descParser.js
+++ descParser.js
@@ -1,64 +1,70 @@
 const fs = require("fs");
 
-let vsls = JSON.parse(fs.readFileSync('./vsl-descr/fleet_A.json'));
+const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let allFleet = [];
 
-vsls = vsls.filter(vsl => {
-  const name = parseText(vsl.description, /<B>MV<\/B>\s"([\d\w\s\-\.']*)"/);
-  if(name !== '')
-    return true
-});
+for(let i = 0; i < abc.length; i++) {
 
-let updatedVsls = vsls.map((vsl, index) => {
+  let vsls = JSON.parse(fs.readFileSync(`./vsl-descr/fleet_${abc[i]}.json`));
 
-  console.log(index, vsl.imo);
-  
-  const descText = vsl.description;
-  
-  const name = parseText(descText, /<B>MV<\/B>\s"([\d\w\s\-\.']*)"/);
-  const type = parseText(descText, /<B>Type :<\/B>([\w\s]*)</);  
-  const subType = parseText(descText, /<B>Sub type :<\/B>([\w\s\-\/]*)</);  
-  const flag = parseText(descText, /<B>Flag :<\/B>([\w\s\(\)&]*)</);
-  const vslClass = parseText(descText, /<B>Class :<\/B>([\w\s]*)</);
-  const dwt = parseText(descText, /<B>DWT\/Draft:<\/B>([\d\,\s]*)M/);
-  const draft = parseText(descText, /<B>DWT\/Draft:<\/B>[\d\,\s]*MT DWT \/([\d\.\s]*) m/);
-  const tpc = parseText(descText, /TPC \/ TPI :([\d\.\s]*)MT/);
-  const loa = parseText(descText, /([\d\.]*) m \(loa\)/);
-  const beam = parseText(descText, /([\d\.]*) m \(beam\)/);  
-  const lbp = parseText(descText, /LBP :\s*([\d\.]*) m/);
-  const grt = parseText(descText, /Int'l tonnage : ([\d\,]*) GT/);  
-  const nrt = parseText(descText, /Int'l tonnage : [\d\,]* GT \/ ([\d\,]*) NT/);
-  const shipowner = parseText(descText, /Shipowner :([\w\s&\-]*)\s\-{2}/);
+  vsls = vsls.filter(vsl => {
+    const name = parseText(vsl.description, /<B>MV<\/B>\s"([\d\w\s\-\.']*)"/);
+    if(name !== '')
+      return true
+  });
 
-  const gears = descText.includes('Gearless')? 'Gearless': 'Geared';
+  allFleet = allFleet.concat(vsls.map((vsl, index) => {
 
-  console.log(index, name);
+    console.log(index, vsl.imo);
     
-  return {
-    name,
-    type,
-    subType, 
-    gears,
-    imo: vsl.imo,
-    callsign: vsl.callsign,
-    mmsi: vsl.mmsi,
-    flag, 
-    vslClass, 
-    dwt, 
-    draft, 
-    tpc, 
-    loa, 
-    beam, 
-    lbp, 
-    grt,
-    nrt, 
-    shipowner, 
-    fullDescription: vsl.description
-  }
-});
+    const descText = vsl.description;    
+    const name = parseText(descText, /<B>MV<\/B>\s"([\d\w\s\-\.']*)"/);
+    const type = parseText(descText, /<B>Type :<\/B>([\w\s]*)</);  
+    const subType = parseText(descText, /<B>Sub type :<\/B>([\w\s\-\/]*)</); 
+    const build = parseText(descText, /<B>Built :<\/B>\s*(\d*)/);
+    const flag = parseText(descText, /<B>Flag :<\/B>([\w\s\(\)&]*)</);
+    const vslClass = parseText(descText, /<B>Class :<\/B>([\w\s]*)</);
+    const dwt = parseText(descText, /<B>DWT\/Draft:<\/B>([\d\,\s]*)M/);
+    const draft = parseText(descText, /<B>DWT\/Draft:<\/B>[\d\,\s]*MT DWT \/([\d\.\s]*) m/);
+    const tpc = parseText(descText, /TPC \/ TPI :([\d\.\s]*)MT/);
+    const loa = parseText(descText, /([\d\.]*) m \(loa\)/);
+    const beam = parseText(descText, /([\d\.]*) m \(beam\)/);  
+    const lbp = parseText(descText, /LBP :\s*([\d\.]*) m/);
+    const grt = parseText(descText, /Int'l tonnage : ([\d\,]*) GT/);  
+    const nrt = parseText(descText, /Int'l tonnage : [\d\,]* GT \/ ([\d\,]*) NT/);
+    const shipowner = parseText(descText, /Shipowner :([\w\s&\-]*)\s\-{2}/);
+    const gears = descText.includes('Gearless')? 'Gearless': 'Geared';
 
-console.log(updatedVsls.length);
+    console.log(index, name);
+      
+    return {
+      name,
+      build,
+      type,
+      subType, 
+      gears,
+      imo: vsl.imo,
+      callsign: vsl.callsign,
+      mmsi: vsl.mmsi,
+      flag, 
+      vslClass, 
+      dwt, 
+      draft, 
+      tpc, 
+      loa, 
+      beam, 
+      lbp, 
+      grt,
+      nrt, 
+      shipowner, 
+      fullDescription: vsl.description
+    }
+  }));
 
-fs.writeFile("./vsl-descr/parsed/fleet_A.json", JSON.stringify(updatedVsls), err => {
+  console.log(allFleet.length);
+}
+
+fs.writeFile("./vsl-descr/parsed/allFleet.json", JSON.stringify(allFleet), err => {
   if (err) throw err;
   
   console.log('The file has been saved!');
